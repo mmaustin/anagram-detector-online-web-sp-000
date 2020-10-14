7c64046ac7d7093bc343ce004f228d5a80290ec4
mccray austin <tubach75@hotmail.com>
2020-10-14 14:10:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,20 @@
 class Anagram
+  attr_accessor :listen
 
+  def initialize(listen)
+    @listen = listen
+  end
 
-  
+  def match(list)
+    array = []
+    list.each do |word|
+      if word.split("").sort == @listen.split("").sort
+        array << word
+      else
+        array
+      end
+    end
+    array
+  end
+
 end
